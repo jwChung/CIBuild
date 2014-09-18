a817d810da2e501bc137fd38db7a954c5302e07b
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-19 08:55:03 +0900

    dev: Addressed humble GitHubTagger.

---
+++ src/CIBuildTasks/GitHubTagger.cs
@@ -0,0 +1,79 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using System;
+    using System.Globalization;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    /// <summary>
+    /// Represents a MSBuild task to create a tag on a repository of GitHub.
+    /// </summary>
+    public class GitHubTagger : Task
+    {
+        private ITaskItem tagInfo;
+
+        /// <summary>
+        /// Gets or sets the tag information.
+        /// </summary>
+        [Required]
+        public ITaskItem TagInfo
+        {
+            get
+            {
+                return this.tagInfo;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.tagInfo = value;
+            }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, executes the task.
+        /// </summary>
+        /// <returns>
+        /// true if the task successfully executed; otherwise, false.
+        /// </returns>
+        public override bool Execute()
+        {
+            this.CreateTag(this.tagInfo);
+
+            this.LogMessageFromText(
+                string.Format(
+                    CultureInfo.CurrentCulture,
+                    "The '{0}' tag was created...",
+                    this.tagInfo.GetMetadata("TagName")),
+                MessageImportance.High);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Creates a tag.
+        /// </summary>
+        /// <param name="tag">
+        /// The tag information.
+        /// </param>
+        protected virtual void CreateTag(ITaskItem tag)
+        {
+        }
+
+        /// <summary>
+        /// Logs message.
+        /// </summary>
+        /// <param name="lineOfText">
+        /// The line of text.
+        /// </param>
+        /// <param name="messageImportance">
+        /// The message importance.
+        /// </param>
+        protected virtual void LogMessageFromText(string lineOfText, MessageImportance messageImportance)
+        {
+            Log.LogMessageFromText(lineOfText, messageImportance);
+        }
+    }
+}
---
+++ test/CIBuildTasks.UnitTest/GitHubTaggerTest.cs
@@ -0,0 +1,103 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Experiment.Xunit;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using Moq;
+    using Moq.Protected;
+    using Ploeh.AutoFixture;
+    using Xunit;
+
+    public class GitHubTaggerTest : TestClassBase<GitHubTagger>, ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Register(() =>
+            {
+                var sut = Mocked.Of<GitHubTagger>();
+
+                sut.ToMock().Protected().Setup(
+                    "CreateTag",
+                    ItExpr.IsAny<ITaskItem>());
+
+                sut.ToMock().Protected().Setup(
+                    "LogMessageFromText",
+                    ItExpr.IsAny<string>(),
+                    ItExpr.IsAny<MessageImportance>());
+
+                return sut;
+            });
+        }
+
+        [Test]
+        public void SutIsTask()
+        {
+            Assert.IsAssignableFrom<Task>(this.Sut);
+        }
+
+        [Test]
+        public void TagInfoIsReadWritable(ITaskItem tagInfo)
+        {
+            Assert.Null(this.Sut.TagInfo);
+            this.Sut.TagInfo = tagInfo;
+            Assert.Equal(tagInfo, this.Sut.TagInfo);
+        }
+
+        [Test]
+        public void TagInfoIsRequired()
+        {
+            this.Properties.Select(x => x.TagInfo).AssertGet<RequiredAttribute>();
+        }
+
+        [Test]
+        public void ExecuteCorrectlyCreateTag(ITaskItem tagInfo)
+        {
+            this.Sut.TagInfo = tagInfo;
+
+            var actual = this.Sut.Execute();
+
+            Assert.True(actual);
+            this.Sut.ToMock().Protected().Verify(
+                "CreateTag",
+                Times.Once(),
+                ItExpr.IsAny<ITaskItem>());
+        }
+
+        [Test]
+        public void ExecuteLogsCorrectMessage(ITaskItem taskInfo, string tagName)
+        {
+            this.Sut.TagInfo = taskInfo.Of(x => x.GetMetadata("TagName") == tagName);
+
+            this.Sut.Execute();
+
+            this.Sut.ToMock().Protected().Verify(
+                "LogMessageFromText",
+                Times.Once(),
+                ItExpr.Is<string>(x => x.Contains(tagName)),
+                MessageImportance.High);
+        }
+
+        [Test]
+        public IEnumerable<ITestCase> ExecuteThrowsIfMetadataNamesOfTagInfoIsIncorrect()
+        {
+            ////var testData = new[]
+            ////{
+            ////    new []{ "Repository", "Referene", "TagName", "ReleaseNotes", "AccessToken", "AuthorName", "AuthorEmail" }
+            ////};
+
+            ////return TestCases.WithArgs(testData).WithAuto<ITaskItem>().Create(
+            ////    (data, tagInfo) =>
+            ////    {
+            ////        tagInfo.Of(x => x.MetadataNames = data);
+            ////    });
+            yield break;
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.TagInfo);
+        }
+    }
+}
