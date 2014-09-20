6f987a238968e562a3eb51d82868ed5527c54710
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-21 00:58:49 +0900

    build: Incremented patch version,
    to indicate that this merged the target assembly with reference
    assemblies.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -10,11 +10,12 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.0.3")]
-[assembly: AssemblyInformationalVersion("0.0.3")]
+[assembly: AssemblyVersion("0.0.4")]
+[assembly: AssemblyInformationalVersion("0.0.4")]
 
 /*
  * Version 0.0.4
  * 
- * - [Patch] Fixed missing reference assemblies.
+ * - [Patch] Used ILMerge to merge the target assembly with reference
+ *   assemblies.
  */
