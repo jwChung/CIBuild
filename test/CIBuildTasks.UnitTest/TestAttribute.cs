3d7218b8617dcefae157bb619a63d36300d0099b
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-23 14:34:23 +0900

    build: Updated all Experiment packages to v2.0.0-pre08.

--- test/CIBuildTasks.UnitTest/TestAttribute.cs
+++ test/CIBuildTasks.UnitTest/TestAttribute.cs
@@ -1,18 +1,17 @@
 ﻿namespace Jwc.CIBuildTasks
 {
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
     using Experiment;
+    using Experiment.AutoFixture;
     using Experiment.Xunit;
     using Ploeh.AutoFixture;
-    using Ploeh.AutoFixture.AutoMoq;
     using Ploeh.AutoFixture.Kernel;
-    using Ploeh.AutoFixture.Xunit;
     using Xunit.Sdk;
 
     public class TestAttribute : TestBaseAttribute
     {
+        private readonly CIBuildFixtureFactory factory = new CIBuildFixtureFactory();
+
         public TestAttribute() : base(new CompositeTestCommandFactory(
             new TestCaseCommandFactory(),
             new DataAttributeCommandFactory(),
@@ -23,36 +22,7 @@
 
         protected override ITestFixture Create(ITestMethodContext context)
         {
-            var fixture = new Fixture().Customize(new CompositeCustomization(
-                new AutoMoqCustomization(),
-                new OmitAutoPropertiesCustomization(),
-                new ParameterCustomization(context),
-                new TestClassCustomization(context),
-                new SutSpecimenCustomization(context)));
-
-            return new TestFixture(fixture);
-        }
-
-        private class TestFixture : ITestFixture
-        {
-            private readonly ISpecimenContext context;
-
-            public TestFixture(IFixture fixture)
-            {
-                this.InjectTestFixture(fixture);
-                this.context = new SpecimenContext(fixture);
-            }
-
-            public object Create(object request)
-            {
-                return this.context.Resolve(request);
-            }
-
-            private void InjectTestFixture(IFixture fixture)
-            {
-                fixture.Inject(this);
-                fixture.Inject<ITestFixture>(this);
-            }
+            return this.factory.Create(context);
         }
 
         private class ForceFixtureCommandFactory : ITestCommandFactory
@@ -63,61 +33,32 @@
             }
         }
 
-        private class ForceFitxtureContext : ITestCommandContext
+        private class ForceFitxtureContext : ParameterizedCommandContext
         {
             private readonly ITestFixtureFactory factory;
-            private readonly ITestCommandContext commandContext;
 
             public ForceFitxtureContext(IMethodInfo testMethod, ITestFixtureFactory factory)
+                : base(testMethod, factory, new object[0])
             {
                 this.factory = factory;
-                this.commandContext = new TestCommandContext(testMethod, factory, new object[0]);
             }
 
-            public IMethodInfo TestMethod
+            public override ITestMethodContext GetMethodContext(object testObject)
             {
-                get { return this.commandContext.TestMethod; }
-            }
-
-            public IEnumerable<object> GetArguments(ITestMethodContext context)
-            {
-                return this.commandContext.GetArguments(context);
-            }
-
-            public ITestMethodContext GetMethodContext(object testObject)
-            {
-                var methodContext = this.commandContext.GetMethodContext(testObject);
+                var methodContext = base.GetMethodContext(testObject);
                 this.factory.Create(methodContext);
                 return methodContext;
             }
         }
 
-        private class OmitAutoPropertiesCustomization : ICustomization
+        private class CIBuildFixtureFactory : TestFixtureFactory
         {
-            public void Customize(IFixture fixture)
+            protected override ICustomization GetCustomization(ITestMethodContext context)
             {
-                fixture.OmitAutoProperties = true;
-            }
-        }
-
-        private class ParameterCustomization : ICustomization
-        {
-            private readonly ITestMethodContext context;
-
-            public ParameterCustomization(ITestMethodContext context)
-            {
-                this.context = context;
-            }
-
-            public void Customize(IFixture fixture)
-            {
-                var customization = new CompositeCustomization(
-                    this.context.ActualMethod.GetParameters().SelectMany(
-                        p => p.GetCustomAttributes(typeof(CustomizeAttribute), false)
-                            .Cast<CustomizeAttribute>()
-                            .Select(a => a.GetCustomization(p))));
-
-                fixture.Customize(customization);
+                return new CompositeCustomization(
+                    base.GetCustomization(context),
+                    new TestClassCustomization(context),
+                    new SutSpecimenCustomization(context));
             }
         }
 
