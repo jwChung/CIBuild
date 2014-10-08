7a111f8d267287b3222d5b4b31c1198dfc0dd0e7
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-08 12:46:49 +0900

    build: Incremented minor version,
    to indicate that this improved buld scripts.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,18 +10,13 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyInformationalVersion("0.3.0")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyInformationalVersion("0.4.0")]
 
 /*
- * Version 0.3.0
+ * Version 0.4.0
  * 
- * - [Major] Removed code-analysis rule sets as rule sets are specified by
- *   projects. This change is breaking-change but is treated as non-major
- *   version release because this release is based on major version zero.
+ * - [Major] Renamed the build property 'SolutionDir' to 'RootDir'.
  * 
- * - [Major] Made build properties as project and the old build project as
- *   targets. to enable a target solution to have only the build project file.
- *   This change is breaking-change but is treated as non-major version release
- *   because this release is based on major version zero.
+ * - [Patch] Fixed RootDir as absolute path.
  */
