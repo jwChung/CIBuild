0323f46e1e3d92f47474cef67546cf672742ef9e
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-16 16:10:07 +0900

    test: Updated Experimentalism to v2.1.0.

--- test/CIBuildTasks.UnitTest/TestAttribute.cs
+++ test/CIBuildTasks.UnitTest/TestAttribute.cs
@@ -5,23 +5,19 @@
     using Experiment.AutoFixture;
     using Experiment.Xunit;
     using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoMoq;
     using Ploeh.AutoFixture.Kernel;
     using Xunit.Sdk;
 
     public class TestAttribute : TestBaseAttribute
     {
-        private readonly CIBuildFixtureFactory factory = new CIBuildFixtureFactory();
         private readonly RunOn runOn;
 
         public TestAttribute() : this(RunOn.Any)
         {
         }
 
-        public TestAttribute(RunOn runOn) : base(new CompositeTestCommandFactory(
-            new TestCaseCommandFactory(),
-            new DataAttributeCommandFactory(),
-            new ParameterizedCommandFactory(),
-            new ForceFixtureCommandFactory()))
+        public TestAttribute(RunOn runOn)
         {
             this.runOn = runOn;
         }
@@ -50,45 +46,13 @@
 
         protected override ITestFixture Create(ITestMethodContext context)
         {
-            return this.factory.Create(context);
-        }
-
-        private class ForceFixtureCommandFactory : ITestCommandFactory
-        {
-            public IEnumerable<ITestCommand> Create(
-                IMethodInfo testMethod, ITestFixtureFactory fixtureFactory)
-            {
-                yield return new ParameterizedCommand(
-                    new ForceFitxtureContext(testMethod, fixtureFactory));
-            }
-        }
-
-        private class ForceFitxtureContext : ParameterizedCommandContext
-        {
-            private readonly ITestFixtureFactory factory;
-
-            public ForceFitxtureContext(IMethodInfo testMethod, ITestFixtureFactory factory)
-                : base(testMethod, factory, new object[0])
-            {
-                this.factory = factory;
-            }
-
-            public override ITestMethodContext GetMethodContext(object testObject)
-            {
-                var methodContext = base.GetMethodContext(testObject);
-                this.factory.Create(methodContext);
-                return methodContext;
-            }
-        }
-
-        private class CIBuildFixtureFactory : TestFixtureFactory
-        {
-            protected override ICustomization GetCustomization(ITestMethodContext context)
-            {
-                return new CompositeCustomization(
-                    base.GetCustomization(context),
-                    new MockedSutCustomization());
-            }
+            var customization = new CompositeCustomization(
+                new AutoMoqCustomization(),
+                new OmitAutoPropertiesCustomization(),
+                new TestParametersCustomization(context.ActualMethod.GetParameters()),
+                new MockedSutCustomization());
+            var fixture = new Fixture().Customize(customization);
+            return new TestFixture(fixture);
         }
 
         private class MockedSutCustomization : ICustomization
