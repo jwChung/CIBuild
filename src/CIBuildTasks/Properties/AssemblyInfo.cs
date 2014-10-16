326a97e9303a8ca1c956caa9309625a3a05dfe8d
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-16 16:45:40 +0900

    buid: Removed GUID from assembly information.

--- src/CIBuildTasks/Properties/AssemblyInfo.cs
+++ src/CIBuildTasks/Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("CIBuildTasks")]
 [assembly: AssemblyDescription("")]
 [assembly: CLSCompliant(true)]
-[assembly: Guid("f9c7a175-00a8-4e23-b330-8d7aa06c77ef")]
