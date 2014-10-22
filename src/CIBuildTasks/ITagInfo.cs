a80d23a1436e39fe9a40c9a93cf85b3636d70f69
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 14:38:26 +0900

    dev: Introduced ITagInfo.

---
+++ src/CIBuildTasks/ITagInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    public interface ITagInfo
+    {
+        string AccessToken { get; }
+
+        string Owner { get; }
+
+        string Repository { get; }
+
+        string RefOrSha { get; }
+
+        string TagName { get; }
+
+        string ReleaseNotes { get; }
+
+        string AuthorName { get; }
+
+        string AuthorEmail { get; }
+    }
+}
