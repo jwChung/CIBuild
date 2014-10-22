5399a6e68606332977da6b64b29510c16f31c733
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 15:08:17 +0900

    dev: Introduced GitHubTagger.

---
+++ src/CIBuildTasks/GitHubTagger.cs
@@ -0,0 +1,159 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using System;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    public class GitHubTagger : Task, ITagInfo
+    {
+        private string accessToken;
+        private string owner;
+        private string repository;
+        private string refOrSha;
+        private string tagName;
+        private string releaseNotes;
+        private string authorName;
+        private string authorEmail;
+
+        [Required]
+        public string AccessToken
+        {
+            get
+            {
+                return this.accessToken;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.accessToken = value;
+            }
+        }
+
+        [Required]
+        public string Owner
+        {
+            get
+            {
+                return this.owner;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.owner = value;
+            }
+        }
+
+        [Required]
+        public string Repository
+        {
+            get
+            {
+                return this.repository;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.repository = value;
+            }
+        }
+
+        [Required]
+        public string RefOrSha
+        {
+            get
+            {
+                return this.refOrSha;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.refOrSha = value;
+            }
+        }
+
+        [Required]
+        public string TagName
+        {
+            get
+            {
+                return this.tagName;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.tagName = value;
+            }
+        }
+
+        [Required]
+        public string ReleaseNotes
+        {
+            get
+            {
+                return this.releaseNotes;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.releaseNotes = value;
+            }
+        }
+
+        [Required]
+        public string AuthorName
+        {
+            get
+            {
+                return this.authorName;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.authorName = value;
+            }
+        }
+
+        [Required]
+        public string AuthorEmail
+        {
+            get
+            {
+                return this.authorEmail;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                this.authorEmail = value;
+            }
+        }
+
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/CIBuildTasks.UnitTest/GitHubTaggerTest.cs
@@ -0,0 +1,116 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Experiment.Xunit;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using Ploeh.Albedo;
+    using Xunit;
+
+    public class GitHubTaggerTest : TestBaseClass
+    {
+        [Test]
+        public void SutIsTask(GitHubTagger sut)
+        {
+            Assert.IsAssignableFrom<Task>(sut);
+        }
+
+        [Test]
+        public void SutIsTagInfo(GitHubTagger sut)
+        {
+            Assert.IsAssignableFrom<ITagInfo>(sut);
+        }
+
+        [Test]
+        public IEnumerable<ITestCase> PropertiesAreReadWritable()
+        {
+            var testData = new[]
+            {
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.AccessToken),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.AccessToken = y)
+                },
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.Owner),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.Owner = y)
+                },
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.Repository),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.Repository = y)
+                },
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.RefOrSha),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.RefOrSha = y)
+                },
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.TagName),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.TagName = y)
+                },
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.ReleaseNotes),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.ReleaseNotes = y)
+                },
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.AuthorName),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.AuthorName = y)
+                },
+                new
+                {
+                    Get = new Func<GitHubTagger, string>(x => x.AuthorEmail),
+                    Set = new Action<GitHubTagger, string>((x, y) => x.AuthorEmail = y)
+                },
+            };
+
+            return TestCases.WithArgs(testData).WithAuto<GitHubTagger, string>().Create(
+                (data, sut, value) =>
+                {
+                    Assert.Null(data.Get(sut));
+                    
+                    data.Set(sut, value);
+
+                    Assert.Equal(value, data.Get(sut));
+                    testData.Except(new[] { data }).All(x =>
+                    {
+                        Assert.NotEqual(value, x.Get(sut));
+                        return true;
+                    });
+                });
+        }
+
+        [Test]
+        public IEnumerable<ITestCase> PropertriesAreRequired()
+        {
+            return TestCases.WithArgs(GetProperties()).Create(property =>
+            {
+                property.AssertGet<RequiredAttribute>();
+            });
+        }
+
+        private static IEnumerable<PropertyInfo> GetProperties()
+        {
+            yield return new Properties<GitHubTagger>().Select(x => x.AccessToken);
+            yield return new Properties<GitHubTagger>().Select(x => x.Owner);
+            yield return new Properties<GitHubTagger>().Select(x => x.Repository);
+            yield return new Properties<GitHubTagger>().Select(x => x.RefOrSha);
+            yield return new Properties<GitHubTagger>().Select(x => x.TagName);
+            yield return new Properties<GitHubTagger>().Select(x => x.ReleaseNotes);
+            yield return new Properties<GitHubTagger>().Select(x => x.AuthorName);
+            yield return new Properties<GitHubTagger>().Select(x => x.AuthorEmail);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            return GetProperties();
+        }
+    }
+}
