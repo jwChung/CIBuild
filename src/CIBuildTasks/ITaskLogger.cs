6a16becdd57647e09ad42721e581e45f6cca5051
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 12:37:00 +0900

    dev: Introduced ITaskLogger.

---
+++ src/CIBuildTasks/ITaskLogger.cs
@@ -0,0 +1,25 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    /// <summary>
+    /// Represents task logger.
+    /// </summary>
+    public interface ITaskLogger
+    {
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="task">
+        /// A task to provide actual logger.
+        /// </param>
+        /// <param name="message">
+        /// Message.
+        /// </param>
+        /// <param name="messageImportance">
+        /// Message importance.
+        /// </param>
+        void Log(Task task, string message, MessageImportance messageImportance);
+    }
+}
