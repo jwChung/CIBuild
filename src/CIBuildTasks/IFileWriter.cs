34b3a6d2e76cc134d7e3218448be2f6b98263a01
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 16:36:20 +0900

    dev: Introduced IFileWriter.

---
+++ src/CIBuildTasks/IFileWriter.cs
@@ -0,0 +1,19 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    /// <summary>
+    /// Represents file writer.
+    /// </summary>
+    public interface IFileWriter
+    {
+        /// <summary>
+        /// Writes byte array to the specified path.
+        /// </summary>
+        /// <param name="path">
+        /// The path.
+        /// </param>
+        /// <param name="value">
+        /// The byte array.
+        /// </param>
+        void Write(string path, byte[] value);
+    }
+}
