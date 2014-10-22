17779b88527619693aca2d68e0e70e3e25af0b8a
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 12:41:23 +0900

    docs: Documented TaskLogger.

--- src/CIBuildTasks/TaskLogger.cs
+++ src/CIBuildTasks/TaskLogger.cs
@@ -4,8 +4,23 @@
     using Microsoft.Build.Framework;
     using Microsoft.Build.Utilities;
 
+    /// <summary>
+    /// Represents task logger.
+    /// </summary>
     public class TaskLogger : ITaskLogger
     {
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="task">
+        /// A task to provide actual logger.
+        /// </param>
+        /// <param name="message">
+        /// Message.
+        /// </param>
+        /// <param name="messageImportance">
+        /// Message importance.
+        /// </param>
         public void Log(Task task, string message, MessageImportance messageImportance)
         {
             if (task == null)
