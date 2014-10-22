094ba7a3e1d9d2d42d9286ed2ec73dd5bf61195a
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 15:31:00 +0900

    dev: Implemented CreateTagCommand.

--- src/CIBuildTasks/CreateTagCommand.cs
+++ src/CIBuildTasks/CreateTagCommand.cs
@@ -1,6 +1,10 @@
 ﻿namespace Jwc.CIBuildTasks
 {
     using System;
+    using System.Globalization;
+    using System.Net;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     public class CreateTagCommand : ICreateTagCommand
     {
@@ -9,7 +13,112 @@
             if (tagInfo == null)
                 throw new ArgumentNullException("tagInfo");
 
-            throw new System.NotImplementedException();
+            using (var tagger = new Tagger(tagInfo))
+            {
+                tagger.CreateTag();
+            }
+        }
+
+        private sealed class Tagger : IDisposable
+        {
+            private readonly WebClient client = new WebClientWithCookies();
+            private readonly ITagInfo tagInformation;
+            private bool disposed;
+
+            public Tagger(ITagInfo tagInformation)
+            {
+                this.tagInformation = tagInformation;
+            }
+
+            public void CreateTag()
+            {
+                var createReferenceInput = new
+                {
+                    @ref = "refs/tags/" + this.tagInformation.TagName,
+                    sha = this.CreateObject()
+                };
+
+                this.Refresh();
+                this.client.UploadString(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "repos/{0}/{1}/git/refs",
+                        this.tagInformation.Owner,
+                        this.tagInformation.Repository),
+                    JsonConvert.SerializeObject(createReferenceInput));
+            }
+
+            public void Dispose()
+            {
+                if (this.disposed)
+                    return;
+
+                this.client.Dispose();
+
+                this.disposed = true;
+            }
+
+            private string CreateObject()
+            {
+                var createObjectInput = new
+                {
+                    tag = this.tagInformation.TagName,
+                    message = this.tagInformation.ReleaseNotes,
+                    @object = this.GetShaForTag(),
+                    type = "commit",
+                    tagger = new
+                    {
+                        name = this.tagInformation.AuthorName,
+                        email = this.tagInformation.AuthorEmail,
+                        date = DateTime.UtcNow.ToString(
+                            "yyyy-MM-ddTHH:mm:sszzz", CultureInfo.CurrentCulture)
+                    }
+                };
+
+                this.Refresh();
+                var createObjectResult = this.client.UploadString(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "repos/{0}/{1}/git/tags",
+                        this.tagInformation.Owner,
+                        this.tagInformation.Repository),
+                    JsonConvert.SerializeObject(createObjectInput));
+
+                return JsonConvert.DeserializeObject<JObject>(createObjectResult)["sha"]
+                    .ToString();
+            }
+
+            private string GetShaForTag()
+            {
+                return this.tagInformation.RefOrSha.StartsWith(
+                    "refs", StringComparison.CurrentCulture)
+                    ? this.GetShaForTagFromReference()
+                    : this.tagInformation.RefOrSha;
+            }
+
+            private string GetShaForTagFromReference()
+            {
+                this.Refresh();
+                var getRefernceResult = this.client.DownloadString(string.Format(
+                    CultureInfo.CurrentCulture,
+                    "repos/{0}/{1}/git/{2}",
+                    this.tagInformation.Owner,
+                    this.tagInformation.Repository,
+                    this.tagInformation.RefOrSha));
+
+                return JsonConvert.DeserializeObject<JObject>(getRefernceResult)["object"]["sha"]
+                    .ToString();
+            }
+
+            private void Refresh()
+            {
+                this.client.BaseAddress = "https://api.github.com";
+                this.client.Headers["Authorization"] = string.Format(
+                    CultureInfo.CurrentCulture, "token {0}", this.tagInformation.AccessToken);
+                this.client.Headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                this.client.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36";
+                this.client.Headers["Accept-Language"] = "en-US,en;q=0.8,el;q=0.6";
+            }
         }
     }
 }
--- test/CIBuildTasks.UnitTest/CreateTagCommandTest.cs
+++ test/CIBuildTasks.UnitTest/CreateTagCommandTest.cs
@@ -1,5 +1,8 @@
 ﻿namespace Jwc.CIBuildTasks
 {
+    using System;
+    using System.Collections.Generic;
+    using Jwc.Experiment.Xunit;
     using Xunit;
 
     public class CreateTagCommandTest : TestBaseClass
@@ -8,6 +11,24 @@
         public void SutIsCreateTagCommand(CreateTagCommand sut)
         {
             Assert.IsAssignableFrom<ICreateTagCommand>(sut);
+        }
+
+        [Test(Skip = "Specify the github AccessToken, explicitly run this test and verify whether the tag is actually created on the github website.")]
+        public void ExecuteCorrectlyCreatesTag(
+            CreateTagCommand sut,
+            ITagInfo tagInfo)
+        {
+            tagInfo.Of(
+                i => i.AccessToken == "*******"
+                    && i.Owner == "jwChung"
+                    && i.Repository == "CIBuild"
+                    && i.ReleaseNotes == "test"
+                    && i.AuthorName == "Jin-Wook Chung"
+                    && i.AuthorEmail == "jwchung@hotmail.com"
+                    && i.RefOrSha == "refs/heads/master"
+                    && i.TagName == Guid.NewGuid().ToString("N"));
+
+            Assert.DoesNotThrow(() => sut.Execute(tagInfo));
         }
     }
 }
