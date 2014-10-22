2e34fc19f46972450d76851d26f79934d3d6e3c6
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 15:18:42 +0900

    dev: Introduced CreateTagCommand.

---
+++ src/CIBuildTasks/CreateTagCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using System;
+
+    public class CreateTagCommand : ICreateTagCommand
+    {
+        public void Execute(ITagInfo tagInfo)
+        {
+            if (tagInfo == null)
+                throw new ArgumentNullException("tagInfo");
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/CIBuildTasks.UnitTest/CreateTagCommandTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using Xunit;
+
+    public class CreateTagCommandTest : TestBaseClass
+    {
+        [Test]
+        public void SutIsCreateTagCommand(CreateTagCommand sut)
+        {
+            Assert.IsAssignableFrom<ICreateTagCommand>(sut);
+        }
+    }
+}
