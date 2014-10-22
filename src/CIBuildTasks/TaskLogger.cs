e5830bf49d9759cabcf6c04bf85f11a3d2c27d14
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-22 12:39:37 +0900

    dev: Introduced TaskLogger.

---
+++ src/CIBuildTasks/TaskLogger.cs
@@ -0,0 +1,20 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using System;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    public class TaskLogger : ITaskLogger
+    {
+        public void Log(Task task, string message, MessageImportance messageImportance)
+        {
+            if (task == null)
+                throw new ArgumentNullException("task");
+
+            if (message == null)
+                throw new ArgumentNullException("message");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/CIBuildTasks.UnitTest/TaskLoggerTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Jwc.CIBuildTasks
+{
+    using Xunit;
+
+    public class TaskLoggerTest : TestBaseClass
+    {
+        [Test]
+        public void SutIsTaskLogger(TaskLogger sut)
+        {
+            Assert.IsAssignableFrom<ITaskLogger>(sut);
+        }
+    }
+}
