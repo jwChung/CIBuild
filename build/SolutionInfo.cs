d68c84f40479d0cdb6ca7fc9e8fa2dcdddef3d19
Jin-Wook Chung <jwChung@hotmail.com>
2015-09-09 03:17:43 +0900

    Incremented pre-release version,
    in order to indicate that this removed framework profiles of nuget specification.

--- build/SolutionInfo.cs
+++ build/SolutionInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.4.5")]
-[assembly: AssemblyInformationalVersion("0.4.5")]
+[assembly: AssemblyVersion("0.4.6")]
+[assembly: AssemblyInformationalVersion("0.4.6-pre01")]
 
 /*
- * Version 0.4.5
+ * Version 0.4.6-pre01
  * 
- * - Fixed error thrown when path in ilmerge-merge script includes a blank.
+ * - Removed nuget framwork profiles to install the packages on any framework.
  */
